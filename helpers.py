1d0ef3c6f1510c37d8c163efe5c928f163c5424d
nbirkes28 <nsbirkes@gmail.com>
2026-02-19 17:15:15 -0600

    Initial commits - setting up the files and adding imports I will be using

---
+++ helpers.py
@@ -0,0 +1,7 @@
+import os
+import random
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime
+from faker import Faker
+
---
+++ main.py
@@ -0,0 +1,22 @@
+# INF601 - Advanced Programming in Python
+
+# Nathan Birkes
+
+# Mini Project 2
+
+
+
+
+
+
+
+
+
+
+
+def main() -> None:
+
+    pass
+
+if __name__ == "__main__":
+    main()
